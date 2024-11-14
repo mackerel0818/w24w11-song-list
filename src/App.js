cc827e1af5829b0b98b5142d8c58a72feb88e674
makerel0818 <minji2700@naver.com>
2024-11-14 14:31:42 +0900

    💩 노래 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <Header />
       <Playlist />
       <Container />
+      <Container2 />
+      <Container3 />
     </div>
   );
 }
@@ -24,6 +26,28 @@ function Container() {
       <a href="https://www.youtube.com/results?search_query=사랑에 연습이 있었다면">
         <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지 1" />
         <div class="song-title">사랑에 연습이 있었다면</div>
+      </a>
+    </div>
+  );
+}
+
+function Container2() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=사건의 지평선">
+        <img src="https://picsum.photos/600/150?random=2" alt="랜덤 이미지 2" />
+        <div class="song-title">사건의 지평선</div>
+      </a>
+    </div>
+  );
+}
+
+function Container3() {
+  return (
+    <div class="container">
+      <a href="https://www.youtube.com/results?search_query=사랑은 늘 도망가">
+        <img src="https://picsum.photos/600/150?random=3" alt="랜덤 이미지 3" />
+        <div class="song-title">사랑은 늘 도망가</div>
       </a>
     </div>
   );
