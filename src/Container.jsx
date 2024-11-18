ba85049577d443bfa065bd052b6f7191add88261
makerel0818 <minji2700@naver.com>
2024-11-18 18:41:11 +0900

    ✂️ Container Component 분리

---
+++ src/Container.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Container(props) {
+  return (
+    <div className="container">
+      <a href={`https://www.youtube.com/results?search_query=${props.title}`}>
+        <img
+          src={`https://picsum.photos/600/150?random=${props.index}`}
+          alt={`랜덤 이미지 ${props.index}`}
+        />
+        <div className="song-title">{props.title}</div>
+      </a>
+    </div>
+  );
+}
