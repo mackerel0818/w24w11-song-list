99a92b168b2a0e1d6228a1e220f6f7471cbd3e47
makerel0818 <minji2700@naver.com>
2024-11-18 18:51:05 +0900

    💄 노래 정보 배열 사용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,36 @@ import "./App.css";
 import Container from "./Container.jsx";
 
 const App = () => {
-  const songs = ["사랑에 연습이 있었다면", "사건의 지평선", "사랑은 늘 도망가"];
+  const songs = [
+    {
+      id: 1,
+      title: "사랑에 연습이 있었다면",
+      singer: "임재현",
+      rating: 5,
+      lyrics: `사랑에 연습이 있었다면
+우리는 달라졌을까
+내가 널 만난 시간 혹은 그 장소
+상황이 달랐었다면 우린 맺어졌을까
+하필 넌 왜 내가 그렇게 철없던 시절에
+나타나서 그렇게 예뻤니
+너처럼 좋은여자가 왜 날 만나서 그런
+과분한 사랑 내게 줬는지`,
+    },
+    {
+      id: 2,
+      title: "사건의 지평선",
+      singer: "윤하",
+      rating: 3,
+      lyrics: null,
+    },
+    {
+      id: 3,
+      title: "어른이 되는 레시피",
+      singer: "심규선",
+      rating: 3,
+      lyrics: null,
+    },
+  ];
 
   return (
     <div>
@@ -20,8 +49,8 @@ const Playlist = (props) => {
   return (
     <div className="playlist">
       <div className="playlist">{props.title}</div>
-      {props.listSong.map((song, index) => (
-        <Container key={index} title={song} index={index} />
+      {props.listSong.map((song) => (
+        <Container key={song.id} song={song} />
       ))}
     </div>
   );
